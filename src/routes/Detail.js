277c1accd566db2b6bae26e6e5b13b3602b21e42
Keunwoo <104286190+kw9212@users.noreply.github.com>
2024-02-26 21:32:33 -0500

    adds detail pages

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,19 +1,46 @@
 import { useParams } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import styles from "./Home.module.css";
+import Movie from "../components/Movie";
 
 function Detail() {
   const { id } = useParams();
-  console.log({ id });
+  const [loading, setLoading] = useState(true);
+  const [info, setInfo] = useState([]);
+  // console.log({ id });
   const getMovie = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
-    console.log(json);
+    // console.log(json);
+    setInfo(json.data.movie);
+    // console.log(json.data.movie);
+    setLoading(false);
   };
   useEffect(() => {
     getMovie();
   }, []);
-  return <h1>Detail</h1>;
+
+  return (
+    <div className={styles.container}>
+      {loading ? (
+        <div className={styles.loader}>
+          <h1>Loading...</h1>
+        </div>
+      ) : (
+        <div className={styles.movies}>
+          <Movie
+            id={info.id}
+            year={info.year}
+            coverImg={info.medium_cover_image}
+            title={info.title}
+            summary={info.description_full}
+            genres={info.genres || []}
+          />
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Detail;
